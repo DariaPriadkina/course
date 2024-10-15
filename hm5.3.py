6f3d9bc4185c844d075b7153534a3105666e0e04
dasha <dashapradkina11@gmail.com>
2024-10-15 19:02:53 +0300

    hm5.3

---
+++ hm5.3.py
@@ -0,0 +1,17 @@
+import string
+
+user_input = input("Введіть рядок: ")
+#Видаляємо знаки пунктуації та пробіли з рядка який ввів користувач
+clean_string = ''.join(char for char in user_input if char not in string.punctuation and char != ' ')
+#Розділяємо рядок на окремі слова
+words = clean_string.split()
+
+hashtag = '#'
+#Додаємо до хештегу кожне слово з великої літери
+for word in words:
+    hashtag += word.title()
+#Перевіряємо довжину рядка та якщо він більший за 140 символів - обмежуємо його до 140
+if len(hashtag) > 140:
+    hashtag = hashtag[:140]
+#Виводимо результат
+print(hashtag)
